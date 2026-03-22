2bac576796391e798a1ca6532c01b4ebf4b379b3
Ana Beatriz Adriano da Silva <ana.beatriz.adriano.silva.1929@gmail.com>
2026-03-22 17:31:45 -0300

    .

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const session = require('express-session');
+const flash = require('connect-flash');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -16,6 +19,19 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'frase_secreta_aqui',   // mude isso!
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 1000 * 60 * 60 * 24 } // 1 dia
+}));
+app.use(flash());
+app.use((req, res, next) => { 
+  res.locals.messages = req.flash();
+  next();
+});
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
